998eb0932bdd2fa7e3d770668eaecf33b8fc26f7
ramaID <rama@qisthi.dev>
2024-05-20 13:46:01 +0700

    init

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+
+it('has correct fillable attributes', function () {
+    $user = new User();
+    expect($user->getFillable())->toBe(['name', 'email', 'password']);
+});
+
+it('has correct hidden attributes', function () {
+    $user = new User();
+    expect($user->getHidden())->toBe(['password', 'remember_token']);
+});
+
+it('has correct cast attributes', function () {
+    $user = new User();
+    expect($user->getCasts())->toBe([
+        'id' => 'int',
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ]);
+});
