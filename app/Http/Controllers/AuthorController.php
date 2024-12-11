b4200e90742b648ff329a0f4293757a721126257
ramaID <ramaID@users.noreply.github.com>
2024-12-11 04:17:15 +0000

    Fix styling

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -10,37 +10,41 @@ class AuthorController extends Controller
 {
     public function __construct(
         protected AuthorService $service
-    ) {
-    }
+    ) {}
 
     public function index()
     {
         $authors = $this->service
-        ->getAll();
+            ->getAll();
+
         return response()->json($authors);
     }
 
     public function show($id)
     {
         $author = $this->service->getById($id);
+
         return response()->json($author);
     }
 
     public function store(StoreAuthorRequest $request)
     {
         $author = $this->service->create($request->toDto());
+
         return response()->json($author, 201);
     }
 
     public function update(UpdateAuthorRequest $request, $id)
     {
         $author = $this->service->update($request->toDto());
+
         return response()->json($author);
     }
 
     public function destroy($id)
     {
         $this->service->delete($id);
+
         return response()->json(null, 204);
     }
 }
